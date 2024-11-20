31ee904ca503e44915afd4dd101939b4c6d47dda
Leroy <ntruderleroy@gmail.com>
2024-11-20 05:09:21 +0300

    more vuex tings

--- src/components/ui/CoverPreview.vue
+++ src/components/ui/CoverPreview.vue
@@ -1,76 +1,76 @@
 <template>
-    <div class="modal" ref="modal">
-        <div class="modal-content">
-            <Icon
-                @click="closePreview"
-                icon="carbon:close-filled"
-                width="25"
-                class="icon"
-            />
-            <img :src="blogCoverPhoto" alt="" />
-        </div>
-    </div>
+	<div class="modal" ref="modal">
+		<div class="modal-content">
+			<Icon
+				@click="closePreview"
+				icon="carbon:close-filled"
+				width="25"
+				class="icon"
+			/>
+			<img :src="blogCoverPhoto" alt="" />
+		</div>
+	</div>
 </template>
 
 <script setup>
-    import { computed } from "vue";
-    import { useStore } from "vuex";
-    import { Icon } from "@iconify/vue";
+	import { computed } from "vue";
+	// import { useStore } from "vuex";
+	import { Icon } from "@iconify/vue";
 
-    const store = useStore();
+	const store = useStore();
 
-    const closePreview = () => {
-        store.commit("openPhotoPreview");
-    };
+	const closePreview = () => {
+		store.commit("openPhotoPreview");
+	};
 
-    const blogCoverPhoto = computed(() => {
-        return store.state.blogPhotoFileURL;
-    });
+	const blogCoverPhoto = computed(() => {
+		return store.state.blogPhotoFileURL;
+	});
 </script>
 
 <style lang="scss" scoped>
-    .modal {
-        top: 0;
-        background-color: rgba(0, 0, 0, 0.5);
-        z-index: 99;
-        position: fixed;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100%;
-        width: 100%;
+	.modal {
+		top: 0;
+		background-color: rgba(0, 0, 0, 0.5);
+		z-index: 99;
+		position: fixed;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
+		width: 100%;
 
-        .modal-content {
-            display: flex;
-            justify-content: center;
-            position: relative;
-            border-radius: 12px;
-            background-color: #fff;
-            height: 400px;
-            width: 600px;
-            padding: 50px;
-            box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
-                0 2px 4px -1px rgba(0, 0, 0, 0.06);
+		.modal-content {
+			display: flex;
+			justify-content: center;
+			position: relative;
+			border-radius: 12px;
+			background-color: #fff;
+			height: 400px;
+			width: 600px;
+			padding: 50px;
+			box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+				0 2px 4px -1px rgba(0, 0, 0, 0.06);
 
-            .icon {
-                width: 24px;
-                height: auto;
-                cursor: pointer;
-                font-size: 24px;
-                position: absolute;
-                top: 15px;
-                right: 15px;
-                color: #303030;
-            }
+			.icon {
+				width: 24px;
+				height: auto;
+				cursor: pointer;
+				font-size: 24px;
+				position: absolute;
+				top: 15px;
+				right: 15px;
+				color: #303030;
+			}
 
-            img {
-                margin-top: 16px;
-                display: block;
-                width: auto;
-                height: 100%;
-                box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
-                    0 2px 4px -1px rgba(0, 0, 0, 0.06);
-            }
-        }
-    }
+			img {
+				margin-top: 16px;
+				display: block;
+				width: auto;
+				height: 100%;
+				box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+					0 2px 4px -1px rgba(0, 0, 0, 0.06);
+			}
+		}
+	}
 </style>
