0adbe25d641871aea54f6304913016f473b42c08
Leroy <ntruderleroy@gmail.com>
2024-12-17 05:22:32 +0300

    firebase try fix

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-	appId: import.meta.env.VITE_FIREBASE_APP_ID,
+	apiKey: "AIzaSyB1CbJBHm_gTABXr2wVEyxxPeUjRuGAj8U",
+	authDomain: "kobi-blogs-app.firebaseapp.com",
+	projectId: "kobi-blogs-app",
+	storageBucket: "kobi-blogs-app.appspot.com",
+	messagingSenderId: "817354330221",
+	appId: "1:817354330221:web:28660fd3a772e58b5def64",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
